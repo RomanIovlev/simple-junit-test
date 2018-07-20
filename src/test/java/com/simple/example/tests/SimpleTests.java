e626dd4a68899daf8ef72dc0999d6cd9e99fba6f
Supreme85 <romanyister@gmail.com>
2018-07-20 13:23:33 +0300

    fix

--- src/test/java/com/simple/example/tests/SimpleTests.java
+++ src/test/java/com/simple/example/tests/SimpleTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FCTServiceServiceTests implements ILog {
+public class SimpleTests implements ILog {
 
     @Test
     public void simpleTest() {
